930f1c3d47c3cef5db21c07929af7ec6ccc98db6
[Albalicia Espino] <Albaliciae@gmail.com>
2024-01-17 20:32:02 -0600

    Code

---
+++ main.py
@@ -0,0 +1,6 @@
+#Write a program to find the maximum and minimum values in a list.
+numbers = [5,10,15,20,25]
+max_number = max(numbers)
+min_number = min(numbers)
+print(max_number)
+print(min_number)
